4568c6ac6a11150fddca2548a378e08f5f0afb78
WhyKash01 <itsyash9211@gmail.com>
2024-05-22 22:47:19 +0530

    db

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,15 +1,35 @@
 import { NextResponse } from "next/server";
+import User from "@prisma/client"
+import { PrismaClient } from '@prisma/client';
 
-export function POST(request: any){
-    const user=[{
-        name: "sda",
-        phone: "9456"
-    },
-    {
-        name: "dfaa",
-        phone: "9asd6"
-    }];
-    return NextResponse.json(user);
+const prisma = new PrismaClient();
+export async function POST(request: any){
+
+    const newUser = await prisma.user.create({
+        data: {
+            username: "harkirat@gmail.com",
+            id: 1 ,
+            name: "", 
+            password: ""
+  
+        },
+      });
+      console.log(newUser);
+    const name = "yash";
+
+    // const existingUser = await User.findOne({
+    //     username: req.body.username
+    // })
+
+    // const user=[{
+    //     name: "sda",
+    //     phone: "9456"
+    // },
+    // {
+    //     name: "dfaa",
+    //     phone: "9asd6"
+    // }];
+    return NextResponse.json(name);
 }
 export function GET(request: any){
     
@@ -17,3 +37,46 @@ export function GET(request: any){
 export function PUT(request: any){
     
 }
+
+// router.post("/signup", async (req, res) => {
+//     const { success } = signupBody.safeParse(req.body)
+//     if (!success) {
+//         return res.status(411).json({
+//             message: "Email already taken / Incorrect inputs"
+//         })
+//     }
+
+//     const existingUser = await User.findOne({
+//         username: req.body.username
+//     })
+
+//     if (existingUser) {
+//         return res.status(411).json({
+//             message: "Email already taken/Incorrect inputs"
+//         })
+//     }
+
+//     const user = await User.create({
+//         username: req.body.username,
+//         password: req.body.password,
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//     })
+//     const userId = user._id;
+
+//     await Account.create({
+//         userId,
+//         balance: 1 + Math.random() * 10000
+//     })
+
+//     const token = jwt.sign({
+//         userId
+//     }, JWT_SECRET);
+
+//     jwt.
+
+//     res.json({
+//         message: "User created successfully",
+//         token: token
+//     })
+// })
